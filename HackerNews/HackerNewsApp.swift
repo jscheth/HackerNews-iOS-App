6b00ef0fc0ce301a124df841abab2ea1f8a62311
jscheth <sovichea.chet@gmail.com>
2025-04-27 15:09:40 -0700

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Jonathan Cheth on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HackerNewsTests/HackerNewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsTests.swift
+//  HackerNewsTests
+//
+//  Created by Jonathan Cheth on 4/27/25.
+//
+
+import Testing
+@testable import HackerNews
+
+struct HackerNewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
